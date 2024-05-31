36364d807e95735669b5cbeb4f893825ee356232
FaridFehresti <115652523+FaridFehresti@users.noreply.github.com>
2024-05-31 20:50:42 +0330

    fixed the issue with tailwind not wokring

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,22 +1,12 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { AuthOperationService } from './services/auth-operation.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
-export class AuthComponent implements OnDestroy{
-  usersSub: Subscription | null = null;
-  constructor(private authOp: AuthOperationService) {
+export class AuthComponent{
 
-  }
-  getUSers(){
-    this.usersSub = this.authOp.getUsers().subscribe(users => console.log(users))
-  }
 
-  ngOnDestroy(): void {
-    this.usersSub?.unsubscribe()
-  }
+
 }
