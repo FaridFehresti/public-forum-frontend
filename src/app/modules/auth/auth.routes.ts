2f7173114ee3809e3de23316c75a34688b896cad
FaridFehresti <115652523+FaridFehresti@users.noreply.github.com>
2024-05-31 22:12:27 +0330

    added theme palete to the tailwindconfig.js

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -6,21 +6,18 @@ import { RegisterFormComponent } from './components/register-form/register-form.
 import { RessetPasswordFormComponent } from './components/resset-password-form/resset-password-form.component';
 
 const routes: Routes = [
-  { path: "", component: AuthComponent,
-    children: [
-      { path: "login",component:LoginFormComponent },
-      {path:"register",component:RegisterFormComponent},
-      {path:"reset-password",component:RessetPasswordFormComponent}
-
-    ]
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
   },
-  
- 
+  { path: 'login', component: LoginFormComponent },
+  { path: 'register', component: RegisterFormComponent },
+  { path: 'reset-password', component: RessetPasswordFormComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        hookersGreen: '#5A7975',
+        goldenBrown: '#947538',
+        cafNoir: '#48381E',
+        khaki: '#A19B7E',
+        dutchWhite: '#E0DAB4',
+        lawnGreen: '#7EF124',
+        forestGreen: '#42920A',
+        black: '#010100',
+        sgbusGreen: '#6CD122',
+        darkMossGreen: '#356A14',
+      },
+    },
   },
   plugins: [],
-  
-}
-
+};
