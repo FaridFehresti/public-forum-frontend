49030e72b9b24cdde3d5187ccbb60e325fd99661
FaridFehresti <115652523+FaridFehresti@users.noreply.github.com>
2024-06-04 02:30:01 +0330

    created early stages of the home layout with tabs and each tab is a  separate component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,11 +3,19 @@ import { HomeRoutingModule } from './home.routes';
 import { HomeComponent } from './home.component';
 import { MatButtonModule } from '@angular/material/button';
 import { ConfirmPopUpAnimation, ConfirmPopUpComponent } from 'src/app/shared/components/confirm-popup/confirm-popup.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { StatComponent } from './main-layout/components/stat/stat.component';
+import { DataComponent } from './main-layout/components/data/data.component';
+import { InventoryComponent } from './main-layout/components/inventory/inventory.component';
+import { MapComponent } from './main-layout/components/map/map.component';
+import { SocialComponent } from './main-layout/components/social/social.component';
 
 
 
 const MAT = [
-  MatButtonModule
+  MatButtonModule,
+  MatTabsModule,
 ];
 
 const PAGES = [
@@ -15,6 +23,12 @@ const PAGES = [
 ];
 const COMPONENTS = [
     HomeComponent,
+    MainLayoutComponent,
+    StatComponent,
+    DataComponent,
+    InventoryComponent,
+    MapComponent,
+    SocialComponent
     
 ]
 
