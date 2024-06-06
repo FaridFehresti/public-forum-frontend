36e841be474bdc52b74930f781a575331cce90bd
FaridFehresti <115652523+FaridFehresti@users.noreply.github.com>
2024-06-07 02:20:02 +0330

    added configuration to the stat route layout this includes adding empty cards and grid layout

--- src/app/modules/stat/stat.module.ts
+++ src/app/modules/stat/stat.module.ts
@@ -4,15 +4,23 @@ import { StatComponent } from './stat.component';
 import { StatRoutingModule } from './stat.routes';
 import { ClockComponent } from 'src/app/shared/components/clock/clock.component';
 import { ThreeDModelComponent} from './components/three-d-model/three-d-model.component'
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 const COMPONENTS = [
     ThreeDModelComponent,
     StatComponent
 ]
-
+const MAT = [
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+]
 @NgModule({
   declarations: [...COMPONENTS],
   imports: [
+    ...MAT,
     CommonModule,
     StatRoutingModule,
     ClockComponent,
