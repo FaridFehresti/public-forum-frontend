998d669c924b61b9fff42a3bf7d6f0a6c58ce6d2
FaridFehresti <115652523+FaridFehresti@users.noreply.github.com>
2024-06-07 00:38:11 +0330

    added 3d model and configured positioning and event binding

--- src/app/modules/stat/stat.module.ts
+++ src/app/modules/stat/stat.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { StatComponent } from './stat.component';
 import { StatRoutingModule } from './stat.routes';
 import { ClockComponent } from 'src/app/shared/components/clock/clock.component';
+import { ThreeDModelComponent} from './components/three-d-model/three-d-model.component'
+
+const COMPONENTS = [
+    ThreeDModelComponent,
+    StatComponent
+]
 
 @NgModule({
-  declarations: [StatComponent],
+  declarations: [...COMPONENTS],
   imports: [
     CommonModule,
     StatRoutingModule,
